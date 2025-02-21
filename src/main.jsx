6636891156d1ffefbe63d9d149675ea1d55397eb
SartDv <munozalejandro531@gmail.com>
2025-02-21 01:01:23 -0500

    Se añade un layout simple

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
-import { StrictMode } from "react";
+//import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 import Home from "./pages/home/Home.jsx";
-import App from "./App.jsx";
+//import App from "./App.jsx";
+import Layout from "./layout/Layout.jsx"
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Layout>
   </BrowserRouter>
+  
 );
