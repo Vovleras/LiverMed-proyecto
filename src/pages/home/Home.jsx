97c9a642e36ba52ac75a4d3c8d54918da2a3fb5b
SartDv <munozalejandro531@gmail.com>
2025-05-05 15:42:02 -0500

    avance pagina inicio

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,21 @@
-import "./Home.css";
+import { useMemo } from "react";
 import { Suspense } from "react";
+import { modelos } from "../../models/modelsMap";
 import Carousel from './Carrusel';
 import { images } from '../../data/images';
+import "./Home.css";
+
+
+const getRandomModelo1 = () => {
+  const enfermedades = Object.keys(modelos);
+  const random = Math.floor(Math.random() * enfermedades.length);
+  const enfermedad = enfermedades[random];
+  return modelos[enfermedad].Modelo1;
+};
 
 const Home = () => {
+  const ModeloAleatorio = useMemo(() => getRandomModelo1(), []);
+
   return (
     <div>
       <section className="homePresentation">
@@ -12,9 +24,10 @@ const Home = () => {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum efficitur posuere nibh sit amet lacinia. Aliquam posuere, mi in suscipit ultrices, velit diam mollis quam, id rutrum risus mauris nec ligula. Pellentesque dignissim blandit mauris, eget interdum lectus convallis at.</p>
           <button className="BotonAprende">Aprende ahora</button>
         </div>
-        <div className="modeloSec">
-          <Suspense fallback={<div>Cargando modelo 3d</div>}>      
-              <img src="/imagenes/liver.png" alt="liver" />
+
+        <div className="presentationModel">
+          <Suspense fallback={<div>Cargando modelo 3D...</div>}>
+            <ModeloAleatorio />
           </Suspense>
         </div>
       </section>
@@ -30,8 +43,8 @@ const Home = () => {
         <h1>Enfermedades comunes</h1>
         <Carousel images={images} />
       </section>
-
     </div>
   );
 };
+
 export default Home;
