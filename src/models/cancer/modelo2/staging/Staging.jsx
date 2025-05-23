eff29c2918f34176e658a1751cc7a570ae351cba
Vovleras <victoria.volveras@correounivalle.edu.co>
2025-05-23 11:17:16 -0500

    Update Staging.jsx

--- src/models/cancer/modelo2/staging/Staging.jsx
+++ src/models/cancer/modelo2/staging/Staging.jsx
@@ -1,18 +1,8 @@
 import { Cloud, Environment, Sparkles, Stars } from "@react-three/drei";
-import { useMemo } from "react";
+
 const Staging = () => {
   return (
     <>
-      {/* Fondo que coincide exactamente */}
-      {/* <Sparkles
-        count={30}
-        speed={1.8}
-        color="#b1b6c8"
-        size={3.5}
-        scale={[8, 8, 8]}
-        noise={1}
-        opacity={2.6}
-      /> */}
       <Sparkles
         count={2}
         size={7}
@@ -20,16 +10,16 @@ const Staging = () => {
         color="beige"
         noise={10}
         opacity={2}
-        position={[0, 2.89, 0]} // Cambia la posición de las chispas
+        position={[0, 2.89, 0]}
       />
 
       <Stars />
 
       <Cloud
-        position={[0, 2.7, 0]} // Aleja la nube en el eje Z (fondo)
-        scale={[0.18, 0.18, 0.18]} // Haz la nube más grande para que cubra el fondo
+        position={[0, 2.7, 0]}
+        scale={[0.18, 0.18, 0.18]}
         opacity={0.7}
-        volume={3.5} // Aumenta el volumen para que la nube sea más densa
+        volume={3.5}
       />
 
       <Environment preset="sunset" />
